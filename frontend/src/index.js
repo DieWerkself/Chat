d530faef8e86bdd7b6fab84ad908397db3f6f685
diewerkself <redead29@gmail.com>
2023-06-24 21:50:02 +0300

    Add Login Form refactor

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,8 @@ import init from './init.jsx';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('chat'));
+  /* eslint-disable-next-line */
   root.render(await init());
 };
-
+/* eslint-disable-next-line */
 app();
